0325e8689035d43f9eab9ea6e96e1428c0e6cbc1
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-07-23 11:21:28 -0400

    Refactor server.js for better structure and readability
    The modifications include the removal of the 'http' module and restructuring the server configuration into a separate function. The 'messages' array has been capitalized for constancy with typical constant naming convention. Furthermore, every route setup is now handled by a dedicated 'setupRoutes' function improving the overall readability of the code.

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('http');
 const socketIO = require('socket.io');
 
-const app = express();
-const server = http.Server(app);
-const io = socketIO(server);
-
 const PORT = 3000;
-
-const messages = [
+const MESSAGES = [
     { name: 'Tim', message: 'Hi' },
     { name: 'Jane', message: 'Hello' }
 ];
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+const io = socketIO(app);
 
-app.get('/messages', getMessages);
-app.post('/messages', postMessage);
-
-io.on('connection', onConnection);
-
-server.listen(PORT, () => {
-    console.log('Server is listening on port', server.address().port);
-});
-
-function getMessages(req, res) {
-    res.send(messages);
+function configureServer() {
+    app.use(express.static(__dirname));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    setupRoutes();
 }
 
-function postMessage(req, res) {
-    messages.push(req.body);
+function setupRoutes() {
+    app.get('/messages', getMessages);
+    app.post('/messages', postChatMessage);
+}
+
+function getMessages(req, res) {
+    res.send(MESSAGES);
+}
+
+function postChatMessage(req, res) {
+    MESSAGES.push(req.body);
     io.emit('message', req.body);
     res.sendStatus(200);
 }
 
-function onConnection(socket) {
+io.on('connection', () => {
     console.log('A user connected');
-}
+});
+
+configureServer();
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
